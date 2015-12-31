0cf3c6dbf581ce9f9f743f1a546de58545c85b23
luojunqiang <luojunqiang@gmail.com>
2016-01-01 02:43:12 +0800

    bugfix

--- scripts/shell/yesterday.sh
+++ scripts/shell/yesterday.sh
@@ -2,5 +2,5 @@
 
 day_offset=$1
 day_offset=${day_offset:=-1}
-day=`perl -MPOSIX -le '@now=localtime; $now[3]+='$day_offset'; print strftime("%Y%m%d", @now)'`
+day=`perl -MPOSIX -le '@now=localtime; $now[3]+='$day_offset'; print strftime("%Y%m%d", localtime(mktime(@now)))'`
 echo $day
